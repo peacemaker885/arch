56edb3b2691d58e97705e46c1f55f6794fabfa4d
rp <reyadm@posteo.net>
2021-01-23 21:58:02 -0800

    Sat Jan 23 09:58:02 PM PST 2021

--- installer-encrypted.sh
+++ installer-encrypted.sh
@@ -61,8 +61,7 @@ if [ -d /sys/firmware/efi ]; then
  parted --script "${device}" -- mklabel gpt \
   mkpart ESP fat32 1Mib 129MiB \
   set 1 boot on \
-  mkpart primary linux-swap 129MiB ${swap_end} \
-  mkpart primary ext4 ${swap_end} 100%
+  mkpart primary ext4 129MiB 100%
 
  # Simple globbing was not enough as on one device I needed to match /dev/mmcblk0p1
  # but not /dev/mmcblk0boot1 while being able to match /dev/sda1 on other devices.
