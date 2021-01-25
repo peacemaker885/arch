523d16aae994b3f199d42572f6633dc392d19f88
rp <reyadm@posteo.net>
2021-01-24 16:39:22 -0800

    Sun Jan 24 04:39:22 PM PST 2021

--- installer-encrypted.sh
+++ installer-encrypted.sh
@@ -2,96 +2,6 @@
 # WARNING: this script will destroy data on the selected disk.
 # This script can be run by executing the following:
 
-set -uo pipefail
-trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
-
-# check internet connection
-ping -c1 -q 8.8.8.8
-status=$?
-case $status in
-    0)
-        echo "Internet connection is available. Will now install";
-    ;;
-    1)
-        echo "Network unreachable or host not responding to pings. Please check interet connection.";
-	exit
-    ;;
-    2)
-        echo "No route to host or other error. Please check internet connection";
-	exit
-    ;;
-esac
-
-### Get infomation from user ###
-read -p 'Hostname: ' hostname
-read -p 'Username: ' user
-read -sp 'Password: ' password
-echo
-read -sp 'repeat Password: ' password2
-[[ "$password" == "$password2" ]] || ( echo "Passwords did not match"; exit 1; )
-
-devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
-printf "\n$devicelist\n"
-read -p "Device to install in: " device
-
-MIRRORLIST_URL="https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on"
-
-echo "Updating mirror list"
-curl -sL "$MIRRORLIST_URL" | \
-    sed -e 's/^#Server/Server/' -e '/^#/d' | \
-    tee /etc/pacman.d/mirrorlist
-
-PACKAGES="base linux-lts grub sudo linux-firmware man-db man-pages \
-          vi iwd wpa_supplicant dialog openssh dhcpcd \
-          exfat-utils zip unzip git polkit reflector"
-
-timedatectl set-ntp true
-read -p "Is this a (r)egular install or (e)ncrypted install? (r/e): " type
-case $type in 
-	r)
-		func_standard
-	;;
-	e)
-		func_encrypted
-	;;
-esac
-
-# Hostname
-echo "${hostname}" > /mnt/etc/hostname
-
-# Grant all to wheel members
-arch-chroot /mnt sed -i s/\#\ %wheel\ ALL=\(ALL\)\ ALL/\%wheel\ ALL=\(ALL\)\ ALL/g /etc/sudoers
-arch-chroot /mnt useradd -m $user -G wheel -s /bin/bash
-
-# Settings
-echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
-arch-chroot /mnt sed -i s/\#en_US.UTF-8\ UTF-8/en_US.UTF-8\ UTF-8/g /etc/locale.gen
-arch-chroot /mnt locale-gen
-arch-chroot /mnt ln -sf /usr/share/zoneinfo/US/Pacific /etc/localtime
-arch-chroot /mnt hwclock --systohc
-
-# Make console more readable after install if HIDPI screen
-#if [[ "$hidpi" =~ ^([yY])+$ ]]
-#then
-#    echo "FONT=latarcyrheb-sun32" | sudo tee /mnt/etc/vconsole.conf
-#fi
-
-# Enable networking
-arch-chroot /mnt systemctl enable iwd
-arch-chroot /mnt mkdir /etc/iwd
-echo -e "[General]\nEnableNetworkConfiguration=true" | tee /mnt/etc/iwd/main.conf
-
-# If Dual booting with Windows 10 under MBR
-# pacstrap /mmt os-prober 
-# arch-chroot /mnt mkdir /mnt/windows
-# mount /dev/sda1 /mnt/windows
-# arch-chroot /mnt os-prober
-# arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
-
-echo "$user:$password" | arch-chroot /mnt chpasswd
-echo "root:$password" | arch-chroot /mnt chpasswd
-echo "Base install completed.  Please reboot and enjoy!"
-
 func_standard () {
 	 ### Setup the disk and partitions ###
 	 echo Partitioning Disk
@@ -285,3 +195,93 @@ func_encrypted () {
 	 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 	fi
+
+set -uo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+
+# check internet connection
+ping -c1 -q 8.8.8.8
+status=$?
+case $status in
+    0)
+        echo "Internet connection is available. Will now install";
+    ;;
+    1)
+        echo "Network unreachable or host not responding to pings. Please check interet connection.";
+	exit
+    ;;
+    2)
+        echo "No route to host or other error. Please check internet connection";
+	exit
+    ;;
+esac
+
+### Get infomation from user ###
+read -p 'Hostname: ' hostname
+read -p 'Username: ' user
+read -sp 'Password: ' password
+echo
+read -sp 'repeat Password: ' password2
+[[ "$password" == "$password2" ]] || ( echo "Passwords did not match"; exit 1; )
+
+devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
+printf "\n$devicelist\n"
+read -p "Device to install in: " device
+
+MIRRORLIST_URL="https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on"
+
+echo "Updating mirror list"
+curl -sL "$MIRRORLIST_URL" | \
+    sed -e 's/^#Server/Server/' -e '/^#/d' | \
+    tee /etc/pacman.d/mirrorlist
+
+PACKAGES="base linux-lts grub sudo linux-firmware man-db man-pages \
+          vi iwd wpa_supplicant dialog openssh dhcpcd \
+          exfat-utils zip unzip git polkit reflector"
+
+timedatectl set-ntp true
+read -p "Is this a (r)egular install or (e)ncrypted install? (r/e): " type
+case $type in 
+	r)
+		func_standard
+	;;
+	e)
+		func_encrypted
+	;;
+esac
+
+# Hostname
+echo "${hostname}" > /mnt/etc/hostname
+
+# Grant all to wheel members
+arch-chroot /mnt sed -i s/\#\ %wheel\ ALL=\(ALL\)\ ALL/\%wheel\ ALL=\(ALL\)\ ALL/g /etc/sudoers
+arch-chroot /mnt useradd -m $user -G wheel -s /bin/bash
+
+# Settings
+echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
+arch-chroot /mnt sed -i s/\#en_US.UTF-8\ UTF-8/en_US.UTF-8\ UTF-8/g /etc/locale.gen
+arch-chroot /mnt locale-gen
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/US/Pacific /etc/localtime
+arch-chroot /mnt hwclock --systohc
+
+# Make console more readable after install if HIDPI screen
+#if [[ "$hidpi" =~ ^([yY])+$ ]]
+#then
+#    echo "FONT=latarcyrheb-sun32" | sudo tee /mnt/etc/vconsole.conf
+#fi
+
+# Enable networking
+arch-chroot /mnt systemctl enable iwd
+arch-chroot /mnt mkdir /etc/iwd
+echo -e "[General]\nEnableNetworkConfiguration=true" | tee /mnt/etc/iwd/main.conf
+
+# If Dual booting with Windows 10 under MBR
+# pacstrap /mmt os-prober 
+# arch-chroot /mnt mkdir /mnt/windows
+# mount /dev/sda1 /mnt/windows
+# arch-chroot /mnt os-prober
+# arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "$user:$password" | arch-chroot /mnt chpasswd
+echo "root:$password" | arch-chroot /mnt chpasswd
+echo "Base install completed.  Please reboot and enjoy!"
